a98f322aae82419bb123046af962c34cd0d8c28d
Evgeniy Sokolov <Evgeniy.Sokolov@solarisbank.de>
2023-02-01 22:16:48 +0200

    refactored

--- src/main/java/ua/ithillel/hausaufgabe/hw3/Game.java
+++ src/main/java/ua/ithillel/hausaufgabe/hw3/Game.java
@@ -1,8 +1,5 @@
 package ua.ithillel.hausaufgabe.hw3;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 public class Game {
     private final String puzzle;
 
@@ -14,16 +11,28 @@ public class Game {
         if (puzzle.equalsIgnoreCase(guess)) {
             return new GameResult(puzzle, GameStatus.WIN);
         }
-        var hint = IntStream.range(0, 15)
-                .mapToObj(index -> resolveSymbol(puzzle, guess, index))
-                .collect(Collectors.joining());
-
-        return new GameResult(hint, GameStatus.LOST);
+        return new GameResult(getHint(guess), GameStatus.LOST);
     }
 
+    private String getHint(String guess) {
+        var sb = new StringBuilder();
+        for (int i = 0; i < 15; i++) {
+            sb.append(resolveSymbol(puzzle, guess, i));
+        }
+        return sb.toString();
+    }
+
+/*
+// same hint with Streams
+    private String getHint(String guess) {
+        return IntStream.range(0, 15)
+                .mapToObj(index -> resolveSymbol(puzzle, guess, index))
+                .collect(joining());
+    }
+ */
+
     private String resolveSymbol(String puzzle, String guess, int index) {
-        if (index < puzzle.length() && index < guess.length()
-                && puzzle.charAt(index) == guess.charAt(index)) {
+        if (index < puzzle.length() && index < guess.length() && puzzle.charAt(index) == guess.charAt(index)) {
             return String.valueOf(puzzle.charAt(index));
         }
         return "#";
