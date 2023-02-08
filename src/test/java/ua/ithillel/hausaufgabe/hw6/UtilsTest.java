4f21f1ab1a50bb71486b94d4bd619c7df59aebaa
Evgeniy Sokolov <Evgeniy.Sokolov@solarisbank.de>
2023-02-08 09:32:17 +0200

    fixed

--- src/test/java/ua/ithillel/hausaufgabe/hw6/UtilsTest.java
+++ src/test/java/ua/ithillel/hausaufgabe/hw6/UtilsTest.java
@@ -1,37 +1,26 @@
 package ua.ithillel.hausaufgabe.hw6;
 
-import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ua.ithillel.hausaufgabe.hw6.Utils.countOccurrence;
 
 class UtilsTest {
 
     @Test
     void shouldCountOccurence() {
-        List<String> list = List.of();
+        var list = new ArrayList<String>();
         list.add("item1");
-
-        assertEquals(list.toArray()[0], "item1");
-
         list.add("item2");
         list.add("item3");
         list.add("item4");
         list.add("item1");
         list.add("item1");
 
-        assertEquals(3, Utils.countOccurrence(list, "item1"));
+        assertThat(countOccurrence(list, "item1"), equalTo(3));
     }
 }
 
-        /*
-. Створити метод countOccurance, що приймає на вхід рядковий список як параметр і довільний рядок.
-Список заповнений довільними словами 10-20 штук, які можуть повторюватись у необмеженій кількості.
- Порахувати скільки разів зустрічається переданий рядок як другий аргумент.
- */
