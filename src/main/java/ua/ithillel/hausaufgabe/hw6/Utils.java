e8fd046c30318854c188549a109a836e02e2ad71
Evgeniy Sokolov <Evgeniy.Sokolov@solarisbank.de>
2023-02-08 09:30:13 +0200

    Fixed a test

--- src/main/java/ua/ithillel/hausaufgabe/hw6/Utils.java
+++ src/main/java/ua/ithillel/hausaufgabe/hw6/Utils.java
@@ -5,7 +5,13 @@ import java.util.List;
 public class Utils {
 
     public static int countOccurrence(List<String> list, String item) {
-       return -1;
+        int count = 0;
+        for (String value: list) {
+            if (value.equals(item)) {
+                count++;
+            }
+        }
+        return count;
     }
 
 }
