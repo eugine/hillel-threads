a64b0bc282367f35e6a478ac4b2524fe0aba4f7c
Evgeniy Sokolov <evgeniy.sokolov@matmatch.com>
2023-03-10 00:56:59 +0200

    updateds

--- src/main/java/ua/ithillel/threads/advanced/immutable/RecordRGB.java
+++ src/main/java/ua/ithillel/threads/advanced/immutable/RecordRGB.java
@@ -2,10 +2,10 @@ package ua.ithillel.threads.advanced.immutable;
 
 public record RecordRGB(String name, int red, int green, int blue) {
     public RecordRGB {
-        check(red, green, blue);
+        check();
     }
 
-    private void check(int red, int green, int blue) {
+    private void check() {
         if (red < 0 || red > 255
                 || green < 0 || green > 255
                 || blue < 0 || blue > 255) {
