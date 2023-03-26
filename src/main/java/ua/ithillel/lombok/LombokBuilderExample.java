f8c5a3d41a882168b658e4e75b89f1f497806de8
Evgeniy Sokolov <evgeniy.sokolov@matmatch.com>
2023-03-26 20:09:39 +0300

    Lombok builder

--- src/main/java/ua/ithillel/lombok/LombokBuilderExample.java
+++ src/main/java/ua/ithillel/lombok/LombokBuilderExample.java
@@ -1,10 +1,26 @@
 package ua.ithillel.lombok;
 
-import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 
-@Builder
+//@Builder
 //@Builder(toBuilder = true)
+@RequiredArgsConstructor
 public class LombokBuilderExample {
-    private final String name;
+    private final String firstName;
+    private final String lastName;
     private final Integer age;
+    private final String gender;
+    private final String education;
+    private final Address address;
+
+    @RequiredArgsConstructor
+    public static class Address {
+        private final String address;
+        private final String line1;
+        private final String line2;
+        private final String city;
+        private final String region;
+        private final String country;
+        private final String zipCode;
+    }
 }
