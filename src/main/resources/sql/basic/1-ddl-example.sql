df0166d57a34fdc62ca320c6e27d08aa79be24cf
Evgeniy Sokolov <evgeniy.sokolov@gmail.com>
2023-04-07 12:10:25 +0300

    DB (#28)

---
+++ src/main/resources/sql/basic/1-ddl-example.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+                       id INTEGER PRIMARY KEY,
+                       name TEXT NOT NULL,
+                       email TEXT UNIQUE NOT NULL
+);
+
+ALTER TABLE users ADD COLUMN phone TEXT;
+
+DROP TABLE users;
