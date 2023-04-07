cf2bf391beb9b8f49f7a9bc66f5b1858e3264025
Evgeniy Sokolov <evgeniy.sokolov@matmatch.com>
2023-04-07 11:50:25 +0300

    added sqls

--- run-db.bat
+++ run-db.bat
@@ -1,1 +1,6 @@
-docker run -d -e POSTGRES_USER=hillel -e POSTGRES_PASSWORD=hillel -p 5432:5432 -v postgres-data:/var/lib/postgresql/data postgres:14.2
+docker run -d ^
+  -e POSTGRES_USER=hillel ^
+  -e POSTGRES_PASSWORD=hillel ^
+  -p 5432:5432 ^
+  -v postgres-data:/var/lib/postgresql/data ^
+  postgres:14.2
