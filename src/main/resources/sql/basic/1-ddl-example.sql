b3eac703ec2ad8603298d0d55c18bfca0f08958c
Evgeniy Sokolov <evgeniy.sokolov@gmail.com>
2023-04-10 13:52:31 +0300

    DB continuation (#29)
    Co-authored-by: Evgeniy Sokolov <evgeniy.sokolov@matmatch.com>

--- src/main/resources/sql/basic/1-ddl-example.sql
+++ src/main/resources/sql/basic/1-ddl-example.sql
@@ -1,9 +1,11 @@
-CREATE TABLE users (
-                       id INTEGER PRIMARY KEY,
-                       name TEXT NOT NULL,
-                       email TEXT UNIQUE NOT NULL
+CREATE TABLE users
+(
+    id    INTEGER PRIMARY KEY,
+    name  TEXT        NOT NULL,
+    email TEXT UNIQUE NOT NULL
 );
 
-ALTER TABLE users ADD COLUMN phone TEXT;
+ALTER TABLE users
+    ADD COLUMN phone TEXT;
 
 DROP TABLE users;
