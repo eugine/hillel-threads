b5a8a49e162d9fe4b5b31e9f0ef69c10eca7deb8
Evgeniy Sokolov <evgeniy.sokolov@matmatch.com>
2023-05-02 14:18:52 +0300

    Rest Students

--- src/main/java/ua/ithillel/dao/student/StudentDto.java
+++ src/main/java/ua/ithillel/dao/student/StudentDto.java
@@ -1,12 +1,13 @@
 package ua.ithillel.dao.student;
 
 public record StudentDto(
+        Long id,
         String name,
         String email,
         String major,
         int age) {
 
     static StudentDto from(Student student) {
-        return new StudentDto(student.getName(), student.getEmail(), student.getEmail(), student.getAge());
+        return new StudentDto(student.getId(), student.getName(), student.getEmail(), student.getEmail(), student.getAge());
     }
 }
