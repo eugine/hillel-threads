66b3b7641e6eab15a268b32cb98e050a6085c3cd
Evgeniy Sokolov <evgeniy.sokolov@gmail.com>
2023-05-05 16:49:43 +0300

    Annnotations (#59)
    Co-authored-by: Evgeniy Sokolov <evgeniy.sokolov@matmatch.com>

---
+++ src/main/java/ua/ithillel/annotation/ExampleAnnotation.java
@@ -0,0 +1,12 @@
+package ua.ithillel.annotation;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExampleAnnotation {
+}
