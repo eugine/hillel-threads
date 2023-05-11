1e573c78bd5d289bba4eef4e31065c93f025d237
Evgeniy Sokolov <evgeniy.sokolov@gmail.com>
2023-05-11 08:50:44 +0300

    Added application.properties (#62)
    Co-authored-by: Evgeniy Sokolov <evgeniy.sokolov@matmatch.com>

--- src/main/java/ua/ithillel/spring/core/SpringCoreConfig.java
+++ src/main/java/ua/ithillel/spring/core/SpringCoreConfig.java
@@ -1,10 +1,16 @@
 package ua.ithillel.spring.core;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("ua.ithillel.spring.core")
+@PropertySource("classpath:application.properties")
 public class SpringCoreConfig {
+
+    @Value("${my.property}")
+    private String myProperty;
 
 }
